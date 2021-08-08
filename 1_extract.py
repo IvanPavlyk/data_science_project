e759da31ed74712363c16ab37baec42ba7ca4291
Ivan Pavlyk <ipa11@sfu.ca>
2021-08-08 12:34:56 -0700

    Initial commit

---
+++ 1_extract.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import sys
+import glob
+import names
+from scipy import signal
+import matplotlib.pyplot as plt
+
+def increment_count(count):
+    returncount = count[0]
+    count[0] += 1
+    return returncount % 31
+    
+
+def main():
+    li = []
+    #path = sys.argv[1]
+    path = 'C:\\Users\\vpavl\\CMPT353\\project\\data'
+    #output_file = sys.argv[2]
+    output_file = "combined_data.csv"
+    all_files=glob.glob(path + "/*.csv")
+    
+    #Extract all the data from files and add to an array of DataFrames
+    count = [0]
+    for filename in all_files:
+        attributes = filename.split("_")
+        attributes_gender = attributes[0].split("\\")
+        attributes[0] = attributes_gender[len(attributes_gender) - 1]
+        attributes[len(attributes) - 1] = attributes[len(attributes) - 1].strip('.csv')
+        df = pd.read_csv(filename, index_col=None, header=0)
+        df = df.drop(['ax'], axis=1)
+        df = df.drop(['ay'], axis=1)
+        df = df.drop(['az'], axis=1)
+        df['gender'] = attributes[0]
+        df['age'] = int(attributes[1])
+        df['height'] = int(attributes[2])
+        df['collection type'] = attributes[3]
+        df['name'] = names.get_first_name(gender=attributes[0])
+        df['time_stamp'] = df.apply(lambda row : increment_count(count), axis = 1)
+        df = df.drop(['time'], axis=1)
+        li.append(df)
+    
+    #Butter filtering and example plots of data before and after filtering
+    b, a = signal.butter(3, 0.1, btype='lowpass', analog=False)
+    for i in range(0,2):
+        plt.figure(figsize=(10,5))
+        title = "Before Filtering/After filtering {}".format(li[i]['collection type'][0])
+        plt.title(title)
+        plt.xlabel('Time')
+        plt.ylabel('Acceleration')
+        low_passed = signal.filtfilt(b, a, li[i]['atotal'])
+        plt.plot(li[i]['time_stamp'], li[i]['atotal'], li[i]['time_stamp'], low_passed, 'r-')
+        file_name = "{}_filtering.png".format(i)
+        plt.savefig(file_name)
+       
+    frame = pd.concat(li, axis=0, ignore_index=True)
+    #Final filtering
+    low_passed = signal.filtfilt(b, a, frame['atotal'])
+    frame['filtered'] = low_passed
+    frame.to_csv(output_file, index=False)
+    
+
+if __name__ == '__main__':
+    main()
